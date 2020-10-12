477efb22ab087da4261f73eec8b538786de2d8b7
vnktsh-raju <72740699+vnktsh-raju@users.noreply.github.com>
2020-10-12 17:35:50 +0530

    Create environment.ts

---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyBFgWxM3GnxCzPTXOLhRt7Z8PzMWbBABjM",
+    authDomain: "angular-crud-67a2e.firebaseapp.com",
+    databaseURL: "https://angular-crud-67a2e.firebaseio.com",
+    projectId: "angular-crud-67a2e",
+    storageBucket: "angular-crud-67a2e.appspot.com",
+    messagingSenderId: "80941041366",
+    appId: "1:80941041366:web:6dbfe1280070ca163a6b77"
+  }
+};
