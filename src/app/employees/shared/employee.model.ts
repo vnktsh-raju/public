630c26cb9005ad49f1fb703ef9fa9c1c54cb2192
vnktsh-raju <72740699+vnktsh-raju@users.noreply.github.com>
2020-10-12 17:51:58 +0530

    Create employee.model.ts

---
+++ src/app/employees/shared/employee.model.ts
@@ -0,0 +1,11 @@
+export class Employee {
+    $key : string;
+    name : string;
+    id : string;
+    address : string;
+    role : string;
+    department : string;
+    skillSet : string;
+    dob : string;
+    doj : string;
+}
